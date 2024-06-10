7127db9bd0037492ecb16e1099e295c75037fba7
priyanshu1019 <goutampriyanshu27@gmail.com>
2024-06-10 14:16:43 +0530

    changed our function such that we can pass two parameters

--- app/src/main/java/com/example/createandusefunction/Functions/passingParamenters.kt
+++ app/src/main/java/com/example/createandusefunction/Functions/passingParamenters.kt
@@ -1,9 +1,15 @@
 package com.example.createandusefunction.Functions
 fun main() {
-    println(birthdayGreeting("Rover"))
+    println(birthdayGreeting("Rover" , 21))
+    println(birthdayGreeting("Prianshu" , 22))
 }
-fun birthdayGreeting(name: String): String {
+//fun birthdayGreeting(name: String): String {
+//    val nameGreeting = "Happy Birthday, $name!"
+//    val ageGreeting = "You are now 5 years old!"
+//    return "$nameGreeting\n$ageGreeting"
+//}
+fun birthdayGreeting(name: String, age: Int): String {
     val nameGreeting = "Happy Birthday, $name!"
-    val ageGreeting = "You are now 5 years old!"
+    val ageGreeting = "You are now $age years old!"
     return "$nameGreeting\n$ageGreeting"
 }
